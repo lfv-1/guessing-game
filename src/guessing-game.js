5a36f8c5ce9de1df80911c39dcc51e53826b732c
nikita <nega123497@mail.com>
2022-08-16 11:04:03 +0300

    feat:task completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.round((this.min + this.max) / 2)
+        return this.result
     }
 
     lower() {
-
+        this.max = this.result
+        this.guess()
     }
 
     greater() {
-
+        this.min = this.result
+        this.guess()
     }
 }
 
